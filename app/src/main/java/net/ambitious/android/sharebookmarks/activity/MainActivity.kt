7235751adf6961e6ca93f715dabb07c8d822e824
bvlion <iwai.satoshi@zaim.co.jp>
2020-07-23 14:53:09 +0900

    左メニューOSS ライセンスを実装

--- app/src/main/java/net/ambitious/android/sharebookmarks/activity/MainActivity.kt
+++ app/src/main/java/net/ambitious/android/sharebookmarks/activity/MainActivity.kt
@@ -1,23 +1,25 @@
 package net.ambitious.android.sharebookmarks.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
-import androidx.appcompat.widget.Toolbar
 import android.view.Menu
+import android.view.MenuItem
 import androidx.core.view.GravityCompat
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.toolbar
 import net.ambitious.android.sharebookmarks.BaseActivity
 import net.ambitious.android.sharebookmarks.R
 import net.ambitious.android.sharebookmarks.util.AnalyticsUtils
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), OnNavigationItemSelectedListener {
 
   private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -26,27 +28,22 @@ class MainActivity : BaseActivity() {
     AnalyticsUtils.logStartActivity(firebaseAnalytics, "MainActivity")
 
     setContentView(R.layout.activity_main)
-    val toolbar: Toolbar = findViewById(R.id.toolbar)
     setSupportActionBar(toolbar)
 
-    val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-    val navView: NavigationView = findViewById(R.id.nav_view)
     val navController =
       supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.findNavController()!!
-    appBarConfiguration = AppBarConfiguration(
-        setOf(
-            R.id.nav_home,
-            R.id.nav_gallery
-        ),
-        drawerLayout
-    )
+    appBarConfiguration = AppBarConfiguration(navController.graph, drawer_layout)
     setupActionBarWithNavController(navController, appBarConfiguration)
-    navView.setupWithNavController(navController)
+    nav_view.setupWithNavController(navController)
+    nav_view.setNavigationItemSelectedListener(this)
   }
 
-  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+  override fun onCreateOptionsMenu(menu: Menu) = true.apply {
     menuInflater.inflate(R.menu.main, menu)
-    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    return super.onOptionsItemSelected(item)
   }
 
   override fun onSupportNavigateUp() =
@@ -58,4 +55,15 @@ class MainActivity : BaseActivity() {
     } else {
       super.onBackPressed()
     }
+
+  override fun onNavigationItemSelected(item: MenuItem) = false.apply {
+    when (item.itemId) {
+      R.id.menu_oss_license -> startActivity(
+          Intent(this@MainActivity, OssLicensesMenuActivity::class.java).apply {
+            putExtra("title", getString(R.string.menu_oss_license))
+          }
+      )
+    }
+    drawer_layout.closeDrawer(GravityCompat.START)
+  }
 }
