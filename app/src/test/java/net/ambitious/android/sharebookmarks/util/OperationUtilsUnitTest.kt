44b51b599ad08ba15de75c19abc1ee04e11eec23
bvlion <iwai.satoshi@zaim.co.jp>
2020-09-23 08:21:19 +0900

    suspend に変更した影響を対応

--- app/src/test/java/net/ambitious/android/sharebookmarks/util/OperationUtilsUnitTest.kt
+++ app/src/test/java/net/ambitious/android/sharebookmarks/util/OperationUtilsUnitTest.kt
@@ -1,37 +1,45 @@
 package net.ambitious.android.sharebookmarks.util
 
-import net.ambitious.android.sharebookmarks.util.Const
-import net.ambitious.android.sharebookmarks.util.OperationUtils
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import kotlinx.coroutines.runBlocking
 import org.hamcrest.core.Is.`is`
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.rules.TestRule
 
 class OperationUtilsUnitTest {
+  @get:Rule
+  val rule: TestRule = InstantTaskExecutorRule()
+
   @Test
   fun createThumbnailUrlTest() {
-    // OGP 取得
-    assertThat(
-        OperationUtils.createThumbnailUrl("https://www.ambitious-i.net"),
-        `is`("https://www.ambitious-i.net/img/main.jpg")
-    )
+    runBlocking {
+      // OGP 取得
+      assertThat(
+          OperationUtils.createThumbnailUrl("https://www.ambitious-i.net"),
+          `is`("https://www.ambitious-i.net/img/main.jpg")
+      )
 
-    // favicon 取得 URL 生成
-    assertThat(
-        OperationUtils.createThumbnailUrl("https://bvlion-app.firebaseapp.com"),
-        `is`("${Const.GOOGLE_FAVICON_URL}bvlion-app.firebaseapp.com")
-    )
+      // favicon 取得 URL 生成
+      assertThat(
+          OperationUtils.createThumbnailUrl("https://bvlion-app.firebaseapp.com"),
+          `is`("${Const.GOOGLE_FAVICON_URL}bvlion-app.firebaseapp.com")
+      )
 
-    // null
-    assertNull(OperationUtils.createThumbnailUrl(null))
+      // null
+      assertNull(OperationUtils.createThumbnailUrl(null))
 
-    // URL 形式でない
-    assertNull(OperationUtils.createThumbnailUrl("test"))
+      // URL 形式でない
+      assertNull(OperationUtils.createThumbnailUrl("test"))
 
-    // URL スキーム
-    assertThat(
-        OperationUtils.createThumbnailUrl("slack://channel?team=T12345&id=Cabcde"),
-        `is`("https://a.slack-edge.com/68794/marketing/img/homepage/hp-prospect/unfurl/slack-homepage-unfurl.ja-JP.jpg")
-    )
+      // URL スキーム
+      assertThat(
+          OperationUtils.createThumbnailUrl("slack://channel?team=T12345&id=Cabcde"),
+          `is`("https://a.slack-edge.com/68794/marketing/img/homepage/hp-prospect/unfurl/slack-homepage-unfurl.ja-JP.jpg")
+      )
+    }
   }
 }
