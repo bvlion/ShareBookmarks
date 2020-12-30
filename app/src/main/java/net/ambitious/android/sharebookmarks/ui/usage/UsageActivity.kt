ca6ec43a0b556538a1ffdf3d05b2331ef88a3ba3
bvlion <iwai.satoshi@zaim.co.jp>
2020-12-31 08:54:50 +0900

    使い方を実装

--- app/src/main/java/net/ambitious/android/sharebookmarks/ui/usage/UsageActivity.kt
+++ app/src/main/java/net/ambitious/android/sharebookmarks/ui/usage/UsageActivity.kt
@@ -1,6 +1,27 @@
 package net.ambitious.android.sharebookmarks.ui.usage
 
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import net.ambitious.android.sharebookmarks.R
+import net.ambitious.android.sharebookmarks.databinding.ActivityUsageBinding
 import net.ambitious.android.sharebookmarks.ui.BaseActivity
 
 class UsageActivity : BaseActivity() {
+
+  private lateinit var binding: ActivityUsageBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    binding = ActivityUsageBinding.inflate(layoutInflater)
+
+    setContentView(binding.root)
+    setTitle(R.string.menu_how_to_use)
+  }
+
+  override fun onStart() {
+    super.onStart()
+    binding.usageRecyclerView.layoutManager = LinearLayoutManager(this)
+    binding.usageRecyclerView.adapter = UsageAdapter()
+  }
 }
---
+++ app/src/main/java/net/ambitious/android/sharebookmarks/ui/usage/UsageAdapter.kt
@@ -0,0 +1,84 @@
+package net.ambitious.android.sharebookmarks.ui.usage
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import net.ambitious.android.sharebookmarks.R
+import net.ambitious.android.sharebookmarks.databinding.RowUsageBinding
+
+class UsageAdapter : Adapter<ViewHolder>() {
+
+  private val items =
+    arrayListOf<ViewType>().apply {
+      add(ViewType.SPACE)
+      repeat(IMAGES_COUNT) { add(ViewType.IMAGE) }
+      add(ViewType.SPACE)
+    }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+    if (viewType == ViewType.SPACE.type) {
+      Space(parent)
+    } else {
+      ItemViewHolder(
+          RowUsageBinding.inflate(
+              LayoutInflater.from(parent.context),
+              parent,
+              false
+          )
+      )
+    }
+
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    if (items[position] == ViewType.IMAGE) {
+      (holder as ItemViewHolder).setData(position)
+    }
+  }
+
+  override fun getItemViewType(position: Int) = items[position].type
+
+  override fun getItemCount() = items.size
+
+  companion object {
+    const val IMAGES_COUNT = 6
+  }
+
+  enum class ViewType(val type: Int) {
+    SPACE(1),
+    IMAGE(2)
+  }
+
+  class ItemViewHolder(val binding: RowUsageBinding) : ViewHolder(binding.root) {
+    fun setData(position: Int) {
+      val context = binding.root.context
+      val resources = context.resources
+
+      binding.usageTitle.setText(
+          resources.getIdentifier(
+              "ic_how_to_use_text_main_${position}", "string",
+              context.packageName
+          )
+      )
+
+      binding.usageImage.setImageResource(
+          resources.getIdentifier(
+              "ic_how_to_use_${position}", "drawable",
+              context.packageName
+          )
+      )
+
+      binding.usageText.setText(
+          resources.getIdentifier(
+              "ic_how_to_use_text_sub_${position}", "string",
+              context.packageName
+          )
+      )
+    }
+  }
+
+  class Space(parent: ViewGroup) : RecyclerView.ViewHolder(
+      LayoutInflater.from(parent.context)
+          .inflate(R.layout.row_usage_space, parent, false)
+  )
+}
