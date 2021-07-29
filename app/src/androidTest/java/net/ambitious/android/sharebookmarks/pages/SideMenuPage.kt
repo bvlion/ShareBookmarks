06d10ede3c758d799ff99aebf0731a778feb5ffd
bvlion <iwai.satoshi@zaim.co.jp>
2021-07-30 00:56:09 +0900

    Dependabot 対応として PR は Unit テストと UI テストを実施する

--- app/src/androidTest/java/net/ambitious/android/sharebookmarks/pages/SideMenuPage.kt
+++ app/src/androidTest/java/net/ambitious/android/sharebookmarks/pages/SideMenuPage.kt
@@ -1,11 +1,27 @@
 package net.ambitious.android.sharebookmarks.pages
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.uiautomator.By
+import net.ambitious.android.sharebookmarks.R
 import net.ambitious.android.sharebookmarks.UITestUtils
+import org.hamcrest.core.AllOf
 
 class SideMenuPage {
   fun checkNotification(): HomePage {
-    UITestUtils.waitUtilGoneObject(By.depth())
+    UITestUtils.waitUtilGoneObject(By.res(UITestUtils.PACKAGE_NAME, "loading"))
+
+    val view = Espresso.onView(
+      AllOf.allOf(
+        ViewMatchers.withId(R.id.recycler_view),
+        ViewMatchers.isDisplayed()
+      )
+    )
+
+    // プルリフレッシュ
+    view.perform(ViewActions.swipeDown())
+
     return HomePage()
   }
 }
